95d9d7f8d8e232b356500d44c8680f5ad3934fdc
Eric Pezzulo <eric.pezzulo@gmail.com>
2021-05-13 17:26:29 -0400

    final

--- src/Banner.js
+++ src/Banner.js
@@ -4,7 +4,11 @@ import "./Banner.css";
 function Banner() {
   return (
     <div className="banner">
-      <a href="https://www.instagram.com/barbets_app/" alt="BarBets Instagram">
+      <a
+        href="https://www.instagram.com/barbets_app/"
+        target="_blank"
+        alt="BarBets Instagram"
+      >
         <i class="fab fa-instagram"></i> &nbsp;
         <span>Follow us at @barbets_app</span> &nbsp;
         <i class="fab fa-instagram"></i>
