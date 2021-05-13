a395727e2c2dd1e56db89e9085b8b0f400282a78
Eric Pezzulo <eric.pezzulo@gmail.com>
2021-05-13 13:57:29 -0400

    fixed horizontal scrollbar and spacing

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import React from "react";
 import Header from "./Header";
 import UserExperience from "./UserExperience";
 import AdminPortal from "./AdminPortal";
-import Promotion from "./Promotion";
 import Footer from "./Footer";
 import AboutUs from "./AboutUs";
 import Banner from "./Banner";
 
 function App() {
   return (
-    <div className="container">
+    <div className="app">
       <Banner />
       <Header />
       <UserExperience />
       <AdminPortal />
       <AboutUs />
-      {/* <Promotion /> */}
       <Footer />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -4,11 +4,9 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <img
-        className="header__image"
-        src="./assets/barbetslogo.png"
-        alt="BarBets logo"
-      />
+      <div className="header__image">
+        <img src="./assets/barbetslogo.png" alt="BarBets logo" />
+      </div>
       <h2 className="header__title">
         BarBets - The fun way to promote your bar! Run sports wagers for
         discounts or promotions to your customers.
